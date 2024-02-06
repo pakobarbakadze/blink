df8330e8ffed65edabe129afc9c809a30e2e4dc3
pakobarbakadze <pakobarbakadze@gmail.com>
2024-02-06 15:33:17 +0400

    refactor: graphql module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,7 @@
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { GraphQLModule } from '@nestjs/graphql';
 import { DbModule } from './config/db';
+import { GraphModule } from './config/graphql';
 import { ChatModule } from './modules/chat/chat.module';
 import { PostsModule } from './modules/posts/posts.module';
 import { UsersModule } from './modules/users/users.module';
@@ -13,14 +12,8 @@ import { UsersModule } from './modules/users/users.module';
     PostsModule,
     ChatModule,
     DbModule,
+    GraphModule,
     ConfigModule.forRoot({ isGlobal: true }),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: true,
-      subscriptions: {
-        'graphql-ws': true,
-      },
-    }),
   ],
   controllers: [],
   providers: [],
---
+++ src/config/graphql.ts
@@ -0,0 +1,19 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      subscriptions: {
+        'graphql-ws': true,
+      },
+      buildSchemaOptions: {
+        numberScalarMode: 'integer',
+      },
+    }),
+  ],
+})
+export class GraphModule {}
